a0cf7824404cc619fc475f58decffb3d4a84340a
leddoo <leo@leddoo.net>
2024-02-25 13:31:26 +0100

    struct example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -17,9 +17,11 @@ fn main() {
 
 
     // module.
+    let inst;
     {
         let lib = store.load_module(include_bytes!("./lib.wasm")).unwrap();
         let lib = store.instantiate_module(lib, &Imports::new()).unwrap();
+        inst = lib;
 
         if let Some(add) = store.exported_func::<(i32, i32), i32>(lib, "add") {
             let result = store.call(add, (33, 36)).unwrap();
@@ -47,6 +49,31 @@ fn main() {
         // not sure why. but that's how you define one.
         _ = print_bytes;
         //store.call(print_bytes, (WasmPtr::new(0), WasmSize(128))).unwrap();
+    }
+
+
+
+    // structs.
+    {
+        #[derive(Clone, Copy, Debug, wenjin_derive::CType)]
+        #[repr(C)]
+        struct Foo {
+            a: u32,
+            b: WasmPtr<Foo>,
+        }
+
+        // @todo: new_memory.
+        let memory = store.exported_memory(inst, "memory").unwrap();
+        let mut mem = store.memory_view(memory);
+
+        let ptr = WasmPtr::new(128);
+        mem.write(ptr, Foo {
+            a: 42,
+            b: WasmPtr::new(0),
+        }).unwrap();
+
+        let foo = mem.read(ptr).unwrap();
+        println!("read(write(foo)): {:?}", foo);
     }
 }
 
